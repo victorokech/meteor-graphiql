f1e6197028348d3ee0cd6033097d1f7540add47c
Victor Okech <victor.okech@meltwater.org>
2019-01-22 15:25:11 +0000

    Updated main.js to create random Author and Post data

--- server/main.js
+++ server/main.js
@@ -13,6 +13,30 @@ import resolvers from '../imports/api/resolvers';
 
 import { Views } from '../imports/api/tasks';
 
+Meteor.startup(() => {
+  casual.seed(123);
+  db.sync({ force: true }).then(() => {
+    _.times(10, () => {
+      return AuthorModel.create({
+        firstName: casual.first_name,
+        lastName: casual.last_name
+      }).then((author) => {
+        return author.createPost({
+          title: 'A post by ${author.firstName}',
+          text: casual.sentences(3)
+        }).then((post) => {
+          // create some View mocks
+          return Views.rawCollection().update(
+            { postId: post.id },
+            { views: casual.integer(0, 100), postId: post.id },
+            { upsert: true }
+          )
+        })
+      })
+    })
+  })
+})
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
